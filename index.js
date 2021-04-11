ce23d2134855bdbb1291842a35e5624b52c3a2ad
Saqlain Khan <iamsaqlain25@gmail.com>
2021-04-11 10:11:45 +0530

    Move private info into dev.js

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyPraser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const config = require("./config/key");
+const { User } = require("./models/user");
+
 mongoose
-  .connect(
-    "mongodb+srv://saqlainkhan:veyvxqTVTMsoQzBn@react-blog.dc0rk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("DB connected"))
   .catch((err) => console.log(err));
 
+app.use(bodyPraser.urlencoded({ extended: true }));
+app.use(bodyPraser.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.json({ "hello ~": "Hi ~~" });
+});
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      userData: doc,
+    });
+  });
 });
 
 app.listen(5000);
